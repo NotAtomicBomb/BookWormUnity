04fb4c50284be8ef1645f2e8639fa70de23f225e
BlackHornet <sgrin27@yahoo.com>
2020-08-25 21:18:38 -0500

    Start

---
+++ Assets/Resources/SceneManagement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void loadScene(){
+        SceneManager.LoadScene(1);
+
+        
+
+
+    }
+    public void killMe(){Application.Quit();}
+}
